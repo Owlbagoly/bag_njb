a4b2d5b68e34ed93021ad479a6125407a2411c3f
Bagoly David <dvdbagoly@gmail.com>
2025-03-15 22:07:36 +0100

    setup.py átírva

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            # 'control_vehicle = bag_njb.control_vehicle:main',
+            'project_node = bag_njb.project_node:main',
         ],
     },
 )
