8ce80b928f36edb9d39f9745287649b656347fc1
Bagoly David <dvdbagoly@gmail.com>
2025-03-15 23:43:51 +0100

    elvileg jó

--- launch/hexagon_launch.py
+++ launch/hexagon_launch.py
@@ -1,16 +1,22 @@
+import launch
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
+        # Indítjuk a turtlesim node-ot
         Node(
-            package='turtlesim',
-            executable='turtlesim_node',
-            name='sim'
+            package='turtlesim',  
+            executable='turtlesim_node',  
+            name='turtlesim',  
+            output='screen'  
         ),
+        
+        # Indítjuk a hexagon_turtle node-ot
         Node(
-            package='bag_njb',  # Az általad megadott csomag neve
-            executable='project_node',  # Az indítani kívánt végrehajtható fájl
-            output='screen',
-        ),
+            package='bag_njb',  #
+            executable='hexagon_turtle',  
+            name='hexagon_turtle',  
+            output='screen'  
+        )
     ])
