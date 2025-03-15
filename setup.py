2a1b39a1eef1248e0ad76ccb3d68b8118a0663bd
Bagoly David <dvdbagoly@gmail.com>
2025-03-15 23:14:08 +0100

    Jó elvileg a setup

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'project_node = bag_njb.project_node:main',
+            'project_node = project_node:main',
         ],
     },
 )
